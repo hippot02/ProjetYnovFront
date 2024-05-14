c0c28d44b86947cd68081d94854c6c398dffd5e7
Hippot <hippot02@gmail.com>
2024-05-14 11:32:20 +0200

    ajout accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,18 @@ import { AccueilComponent } from './components/accueil/accueil.component';
 import { FactoryTarget } from '@angular/compiler';
 import { FactureComponent } from './components/facture/facture.component';
 import { NgForm, NgModel } from '@angular/forms';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,
-    ClientComponent,
-    ProduitComponent,
-    AccueilComponent,
-    FactureComponent,
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet,
+        ClientComponent,
+        ProduitComponent,
+        AccueilComponent,
+        FactureComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'ProjetYnovFront';
--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
 })
